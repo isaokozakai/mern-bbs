9a6358dfede6eca5ba06c70529cd3f15147ee64f
Isao Kozakai <feat130@hotmail.co.jp>
2019-11-19 14:16:38 -0800

    Delete unnecessary code

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Container } from 'reactstrap';
 import ItemForm from './ItemForm';
