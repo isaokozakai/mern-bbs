55a41478a29ef66b2e1f9a32cacb1667390bd6da
Isao Kozakai <feat130@hotmail.co.jp>
2019-11-28 19:19:07 -0800

    Fix the problem with incognito mode

--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,18 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import rootReducer from './reducers';
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
-  applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
-));
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(...middleware)
+  ));
 
 export default store;
 
